f7b5f954fc5f66b1db68da8da4922573d2d5c95d
Tim Steinke <43407247+TimSteinke@users.noreply.github.com>
2023-01-21 18:28:42 +0100

    mapping node structure and deprojection

---
+++ freicar_mapping/src/lib/deprojection.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+from geometry_msgs.msg import PoseStamped
+from sensor_msgs.msg import CameraInfo
+
+# TODO: declare the dependencies in the CMakeList file
+from jsk_recognition_msgs.msg import BoundingBox
+from pyrealsense2 import intrinsics, distortion, rs2_deproject_pixel_to_point
+
+
+def get_relative_pose_from_bbox(
+    bbox: BoundingBox, cam_info: CameraInfo, depth: float
+) -> PoseStamped:
+    """
+    Computes the sign pose relative to the camera frame.
+    -----------
+    Parameters:
+        bbox (jsk_recognition_msgs.msg.BoundingBox): Bounding box message describing the
+                position of the sign (street sign or Aruco marker) in the image plane. Assumes
+                the bbox to be axis-aligned and rectangular, and thus only uses x,y of the
+                message's pose and dimensions and assumes x as the horizontal and y as the
+                vertical axis of the image, with (0,0) being the upper left coordinate.
+        cam_info (sensor_msgs.msg.CameraInfo): Camera intrinsics message
+                published in the .../camera_info topic
+        depth (float): Average distance to the bounding box in meters.
+    --------
+    Returns:
+        sign_pose (PoseStamped): The street sign's pose in the camera frame, timestamped at the
+                same time as the Bounding Box message. Units and coordinate system should be
+                compatible with ROS.
+    """
+    # this just uses the center point of the bounding box for deprojection,
+    # if we want to use non-rectangular boxes we'll need something fancier.
+    center_pixel = [
+        bbox.pose.position.x + (bbox.dimensions.x // 2),
+        bbox.pose.position.y + (bbox.dimensions.y // 2),
+    ]
+
+    # Transform camera information into pyrealsense2 format
+    # See these links for explanation on what these are:
+    # https://medium.com/@yasuhirachiba/converting-2d-image-coordinates-to-3d-coordinates-using-ros-intel-realsense-d435-kinect-88621e8e733a
+    # https://github.com/IntelRealSense/librealsense/wiki/Projection-in-RealSense-SDK-2.0#intrinsic-camera-parameters
+    # https://docs.ros.org/en/melodic/api/sensor_msgs/html/msg/CameraInfo.html
+    pr2_intrinsics = intrinsics()
+    pr2_intrinsics.width = cam_info.width
+    pr2_intrinsics.height = cam_info.height
+    pr2_intrinsics.ppx = cam_info.K[2]
+    pr2_intrinsics.ppy = cam_info.K[5]
+    pr2_intrinsics.fx = cam_info.K[0]
+    pr2_intrinsics.fy = cam_info.K[4]
+    pr2_intrinsics.coeffs = cam_info.D
+
+    if cam_info.distortion_model == "plumb_bob":
+        # the d435 camera messages state the model as plumb_bob, and this:
+        # https://calib.io/blogs/knowledge-base/camera-models
+        # says that Plumb Bob is equivalent to Brown-Conrady.
+        pr2_intrinsics.model = distortion.brown_conrady
+    else:
+        raise NotImplementedError(
+            f"CameraInfo message specified unknown distortion model: {cam_info.distortion_model}"
+        )
+
+    # call to librealsense2 to do the actual deprojection
+    pose_x, pose_y, pose_z = rs2_deproject_pixel_to_point(
+        pr2_intrinsics, center_pixel, depth
+    )
+
+    # use a stamped pose message as an easy way to pass on the timestamp and for possibly
+    # publishing as a debug message.
+    sign_pose = PoseStamped()
+    sign_pose.header.stamp = bbox.header.stamp
+    sign_pose.header.frame_id = cam_info.header.frame_id
+
+    # convert coordinate system to the default ROS way (right-handed). See:
+    # https://github.com/IntelRealSense/librealsense/wiki/Projection-in-RealSense-SDK-2.0#point-coordinates
+    # and
+    # https://www.ros.org/reps/rep-0103.html#axis-orientation
+    sign_pose.pose.position.x = pose_z
+    sign_pose.pose.position.y = -pose_x
+    sign_pose.pose.position.z = -pose_y
+
+    # TODO: set orientation. For this we need to get more than one depth value (e.g. left side
+    # and right side of the bounding box)
+
+    return sign_pose
